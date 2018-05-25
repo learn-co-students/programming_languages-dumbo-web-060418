d5e7264f419202e9f5d3e1f934eb5ee375661a3e
critsmet <36425627+critsmet@users.noreply.github.com>
2018-05-25 10:58:40 -0700

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+refLan = {}
+languages.each do |sty, info|
+  info.each do |lan, det|
+  refLan[lan] ||= det
+  refLan[lan][:style] ||= []
+  refLan[lan][:style] << sty
+end
+end
+return refLan
 end
